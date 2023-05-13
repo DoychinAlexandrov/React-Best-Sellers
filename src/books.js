88b8d253f3e87eb98be87cb5a5539969585a3b40
DoychinAlexandrov <doychin_alexandrov@abv.bg>
2023-05-13 15:37:11 +0300

    commit all

---
+++ src/books.js
@@ -0,0 +1,27 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+
+
+export const books = [
+    {
+        author: 'Glenn Boozan',
+        title: 'There Are Moms Way Worse Than You',
+        img: img1,
+        id: 1,
+    },
+
+    {
+        author: 'James Clear',
+        title: 'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+        img: img2,
+        id: 2,
+    },
+    {
+        author: 'Rami Sethi',
+        title: 'I Will Teach You to Be Rich: No Guilt. No Excuses.',
+        img: img3,
+        id: 3,
+    }
+];
